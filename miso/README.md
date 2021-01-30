d3adb5a5f2561c8e2d212e202e8c872f642120e1
Mark Gerarts <mark.gerarts@gmail.com>
2021-01-30 20:14:40 +0100

    Bind to model, implement add more

--- miso/README.md
+++ miso/README.md
@@ -9,6 +9,10 @@
 - onSubmit met preventDefault lijkt niet te werken - al veel tijd in gestoken zonder oplossing
   - In Functional Programming Slack om hulp gevraagd (30/01 - nog geen antwoord)
   - Blijkt enkel in JSaddle een probleem te zijn
+  - Tijdelijk opgelost door een div ipv een form te gebruiken
+- Rechtstreeks Haskell kunnen gebruiken is wel een groot voordeel
+  - Autocomplete in vscode werkt niet out-of-the-box zoals bij een Stack project
+- Werking doet aan Elm denken, eenvoudig en vlot
 
 ## Usage
 
--- miso/app/Main.hs
+++ miso/app/Main.hs
@@ -10,12 +10,25 @@ import qualified Network.Wai.Handler.Warp         as Warp
 import           Network.WebSockets
 #endif
 import Control.Monad.IO.Class
+import qualified Data.Map as M
 import Miso
-import Miso.String
+import Miso.String hiding (map)
 
-type Model = Int
+data Model = Model {album :: Album} deriving (Eq)
 
-data Action = SubmitForm | NoOp deriving (Show, Eq)
+data Album = Album
+  { name :: String,
+    artist :: String,
+    source :: Source,
+    tags :: [Tag]
+  }
+  deriving (Eq)
+
+data Source = CD | LP | Digital deriving (Show, Eq)
+
+type Tag = String
+
+data Action = SubmitForm | AddTag | NoOp deriving (Show, Eq)
 
 #ifndef __GHCJS__
 runApp :: JSM () -> IO ()
@@ -31,7 +44,7 @@ main :: IO ()
 main = runApp $ startApp App {..}
   where
     initialAction = NoOp
-    model = 0
+    model = demoModel
     update = updateModel
     view = viewModel
     events = defaultEvents
@@ -39,41 +52,82 @@ main = runApp $ startApp App {..}
     mountPoint = Nothing
     logLevel = Off
 
+initialModel :: Model
+initialModel =
+  Model
+    { album =
+        Album
+          { name = "",
+            artist = "",
+            source = CD,
+            tags = []
+          }
+    }
+
+demoModel :: Model
+demoModel =
+  Model
+    { album =
+        Album
+          { name = "AlbumName",
+            artist = "ArtistName",
+            source = Digital,
+            tags = ["First Tag"]
+          }
+    }
+
 updateModel :: Action -> Model -> Effect Action Model
 updateModel NoOp m = noEff m
 updateModel SubmitForm m =
   m <# do consoleLog "The form is submitted" >> pure NoOp
+updateModel AddTag model@(Model {album = album@(Album {..})}) =
+  noEff (model {album = album {tags = tags ++ [""]}})
 
 viewModel :: Model -> View Action
-viewModel x =
-  form_
-    [onSubmit SubmitForm]
-    [ div_
+viewModel (Model {album = Album {..}}) =
+  div_ -- I'd rather use a form + onSubmit here, but JSaddle bugs with preventDefault...
+    []
+    [ link_
+        [ rel_ "stylesheet",
+          href_ "https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"
+        ],
+      h1_ [] [text "Miso"],
+      h2_ [] [text "Album form"],
+      div_
         []
-        [ label_ [for_ "album"] [text "Album"],
-          input_ [id_ "album"]
+        [ label_ [for_ "album"] [text "Name"],
+          input_ [id_ "album", value_ $ toMisoString name]
         ],
       div_
         []
         [ label_ [for_ "artist"] [text "Artist"],
-          input_ [id_ "artist"]
+          input_ [id_ "artist", value_ $ toMisoString artist]
         ],
       div_
         []
         [ label_ [for_ "source"] [text "Source"],
           select_
             [id_ "source"]
-            [ option_ [value_ "cd"] [text "CD"],
-              option_ [value_ "lp"] [text "LP"],
-              option_ [value_ "digital"] [text "Digital"]
+            [ option_ [value_ "cd", selected_ (source == CD)] [text "CD"],
+              option_ [value_ "lp", selected_ (source == LP)] [text "LP"],
+              option_ [value_ "digital", selected_ (source == Digital)] [text "Digital"]
             ]
         ],
-      div_ -- @todo: add more
+      div_
         []
-        [ label_ [for_ "tags"] [text "Tags"],
-          input_ [id_ "tags"]
-        ],
+        ([label_ [] [text "Tags"]] ++ map tagToInput tags ++ [button_ [onClick AddTag] [text "Add more"]]),
       button_
-        []
+        [onClick SubmitForm]
         [text "Submit"]
+    ]
+
+inlineBlock :: Attribute action
+inlineBlock = style_ $ M.singleton "display" "inline-block"
+
+tagToInput :: Tag -> View action
+tagToInput tag =
+  div_
+    []
+    [ input_ [id_ "tags", inlineBlock, value_ $ toMisoString tag],
+      button_ [inlineBlock] [text "Remove"] -- @todo: implement remove
     ]
