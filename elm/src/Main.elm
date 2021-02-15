ab6782850a2a21f187c05a137bec7367d9365177
Mark Gerarts <mark.gerarts@gmail.com>
2021-02-15 15:51:29 +0100

    Update model on every change

--- elm/src/Main.elm
+++ elm/src/Main.elm
@@ -30,12 +30,18 @@ type Source
 
 
 type Msg
-    = NoOp
-    | AddTag
-    | RemoveTag Int
-    | UpdateTag Int String
+    = Update UpdateAlbum
     | SubmitForm
     | FormSubmitted (Result Http.Error ())
+
+
+type UpdateAlbum
+    = UpdateName String
+    | UpdateArtist String
+    | UpdateSource Source
+    | AddTag
+    | UpdateTag Int String
+    | RemoveTag Int
 
 
 main : Program () Model Msg
@@ -67,23 +73,35 @@ exampleAlbum =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none )
-
-        AddTag ->
-            ( { model | album = addTag model.album "" }, Cmd.none )
-
-        RemoveTag i ->
-            ( { model | album = removeTag model.album i }, Cmd.none )
-
-        UpdateTag i newTag ->
-            ( { model | album = updateTag model.album i newTag }, Cmd.none )
-
         SubmitForm ->
             ( model, submitAlbum model.album )
 
         FormSubmitted _ ->
             ( { model | submitted = True }, Cmd.none )
+
+        Update albumUpdate ->
+            let
+                album =
+                    model.album
+            in
+            case albumUpdate of
+                UpdateName name ->
+                    ( { model | album = { album | name = name } }, Cmd.none )
+
+                UpdateArtist artist ->
+                    ( { model | album = { album | artist = artist } }, Cmd.none )
+
+                UpdateSource source ->
+                    ( { model | album = { album | source = source } }, Cmd.none )
+
+                AddTag ->
+                    ( { model | album = addTag album "" }, Cmd.none )
+
+                RemoveTag i ->
+                    ( { model | album = removeTag album i }, Cmd.none )
+
+                UpdateTag i tag ->
+                    ( { model | album = updateTag album i tag }, Cmd.none )
 
 
 addTag : Album -> String -> Album
@@ -134,21 +152,29 @@ viewAlbumForm { album, submitted } =
     div []
         [ div []
             [ label [] [ text "Name" ]
-            , input [ value album.name ] []
+            , input
+                [ value album.name
+                , onInput (\v -> Update (UpdateName v))
+                ]
+                []
             ]
         , div []
             [ label [] [ text "Artist" ]
-            , input [ value album.artist ] []
+            , input
+                [ value album.artist
+                , onInput (\v -> Update (UpdateArtist v))
+                ]
+                []
             ]
         , div []
             [ label [] [ text "Source" ] ]
-        , select []
+        , select [ onInput (\v -> Update (UpdateSource (sourceFromString v))) ]
             (List.map (viewSourceOption album.source) [ CD, LP, Digital ])
         , div []
             (List.concat
                 [ [ label [] [ text "Tags" ] ]
                 , List.indexedMap (\i t -> viewTag i t) album.tags
-                , [ button [ onClick AddTag ] [ text "Add more" ] ]
+                , [ button [ onClick (Update AddTag) ] [ text "Add more" ] ]
                 ]
             )
         , button
@@ -188,18 +214,31 @@ sourceToString source =
             "Digital"
 
 
+sourceFromString : String -> Source
+sourceFromString source =
+    case source of
+        "CD" ->
+            CD
+
+        "LP" ->
+            LP
+
+        _ ->
+            Digital
+
+
 viewTag : Int -> String -> Html Msg
 viewTag i tag =
     div []
         [ input
             [ style "display" "inline-block"
             , value tag
-            , onInput (UpdateTag i)
+            , onInput (\v -> Update (UpdateTag i v))
             ]
             []
         , button
             [ style "display" "inline-block"
-            , onClick (RemoveTag i)
+            , onClick (Update (RemoveTag i))
             ]
             [ text "Remove" ]
         ]
