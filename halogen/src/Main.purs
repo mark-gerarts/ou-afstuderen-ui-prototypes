8b0211c357a7998e8cb9c2f7fefdf45a9c6b3f04
Mark Gerarts <mark.gerarts@gmail.com>
2021-02-25 17:51:00 +0100

    Purs: implement fetching of album from server

--- halogen/src/Main.purs
+++ halogen/src/Main.purs
@@ -3,10 +3,12 @@ module Main where
 import Prelude
 import Affjax as AX
 import Affjax.RequestBody as RequestBody
-import Data.Argonaut (class EncodeJson, encodeJson)
+import Affjax.ResponseFormat as AXRF
+import Data.Argonaut (class DecodeJson, class EncodeJson, decodeJson, encodeJson)
 import Data.Argonaut.Core as J
-import Data.Array (drop, mapWithIndex, take, updateAt)
-import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Array (drop, intercalate, mapWithIndex, take, updateAt)
+import Data.Either (Either(..))
+import Data.Maybe (Maybe(..), fromMaybe, maybe)
 import Effect (Effect)
 import Effect.Aff.Class (class MonadAff)
 import Halogen as H
@@ -25,6 +27,7 @@ main =
 type State
   = { album :: Album
     , submitted :: Boolean
+    , serverResponse :: Maybe Album
     }
 
 type Album
@@ -42,6 +45,16 @@ data Source
   | LP
   | Digital
 
+data Action
+  = AddTag
+  | RemoveTag Int
+  | UpdateTag Int Tag
+  | UpdateName String
+  | UpdateArtist String
+  | UpdateSource Source
+  | SubmitForm
+  | FetchAlbum
+
 derive instance eqSource :: Eq Source
 
 instance showSource :: Show Source where
@@ -52,15 +65,10 @@ instance showSource :: Show Source where
 instance encodeJsonSource :: EncodeJson Source where
   encodeJson = encodeJson <<< show
 
-data Action
-  = NoOp
-  | AddTag
-  | RemoveTag Int
-  | UpdateTag Int Tag
-  | UpdateName String
-  | UpdateArtist String
-  | UpdateSource Source
-  | SubmitForm
+instance decodeJsonSource :: DecodeJson Source where
+  decodeJson json = do
+    string <- decodeJson json
+    Right $ sourceFromString string
 
 component :: forall query input output m. MonadAff m => H.Component HH.HTML query input output m
 component =
@@ -78,11 +86,11 @@ component =
         , tags: [ "Rock" ]
         }
     , submitted: false
+    , serverResponse: Nothing
     }
 
 handleAction :: forall output m. MonadAff m => Action -> H.HalogenM State Action () output m Unit
 handleAction = case _ of
-  NoOp -> H.modify_ \state -> state
   AddTag -> H.modify_ \state -> state { album = addTag state.album "" }
   UpdateTag i tag -> H.modify_ \state -> state { album = updateTag state.album i tag }
   RemoveTag i -> H.modify_ \state -> state { album = removeTag state.album i }
@@ -93,6 +101,13 @@ handleAction = case _ of
     album <- H.gets _.album
     r <- H.liftAff $ AX.post_ "http://localhost:8080" (Just (RequestBody.json (albumToJson album)))
     H.modify_ \state -> state { submitted = true }
+  FetchAlbum -> do
+    r <- H.liftAff $ AX.get AXRF.json "http://localhost:8080/album/1"
+    case r of
+      Left err -> H.modify_ \state -> state
+      Right response -> case albumFromJson response.body of
+        Left err -> H.modify_ \state -> state
+        Right album -> H.modify_ \state -> state { serverResponse = Just album }
 
 addTag :: Album -> Tag -> Album
 addTag album tag = album { tags = album.tags <> [ tag ] }
@@ -108,12 +123,20 @@ removeTag album i = album { tags = take i album.tags <> drop (i + 1) album.tags 
 albumToJson :: Album -> J.Json
 albumToJson = encodeJson
 
+albumFromJson :: J.Json -> Either String Album
+albumFromJson = decodeJson
+
 render :: forall a. State -> HH.HTML a Action
 render state =
   HH.div_ -- `div_ [..]` is a shorthand for `div [] [..]`
     [ HH.h1_ [ HH.text "Purescript + Halogen" ]
     , HH.h2_ [ HH.text "Album form" ]
     , renderAlbumForm state
+    , HH.h2_ [ HH.text "Displaying JSON data" ]
+    , maybe
+        (HH.button [ HE.onClick \_ -> Just FetchAlbum ] [ HH.text "Fetch album from server" ])
+        renderAlbum
+        state.serverResponse
     ]
 
 renderAlbumForm :: forall a. State -> HH.HTML a Action
@@ -171,6 +194,19 @@ renderTag i tag =
         , HE.onClick \_ -> Just (RemoveTag i)
         ]
         [ HH.text "Remove" ]
+    ]
+
+renderAlbum :: forall a. Album -> HH.HTML a Action
+renderAlbum album =
+  HH.dl_
+    [ HH.dt_ [ HH.text "Title:" ]
+    , HH.dd_ [ HH.text album.name ]
+    , HH.dt_ [ HH.text "Artist:" ]
+    , HH.dd_ [ HH.text album.artist ]
+    , HH.dt_ [ HH.text "Source:" ]
+    , HH.dd_ [ HH.text $ show album.source ]
+    , HH.dt_ [ HH.text "Tags:" ]
+    , HH.dd_ [ HH.text $ intercalate ", " album.tags ]
     ]
 
 inlineBlock :: forall a b. HP.IProp a b
